b6f2f67e2b29c0a7d73e152dd38aa712454d7d3b
Bobbanz1 <59128051+Bobbanz1@users.noreply.github.com>
2022-03-10 09:29:01 +0100

    Update conditions.view.php

--- conditions.view.php
+++ conditions.view.php
@@ -27,6 +27,30 @@
         <?php endif; ?>
       </li>
     </ul>
+    <?php if ($task['completed']) : ?>
+      <h2>New Task</h2>
+      <ul>
+        <li>
+          <strong>Task Name: </strong> <?= $new_tasks['title']; ?>
+        </li>
+
+        <li>
+          <strong>Due Date: </strong> <?= $new_tasks['due']; ?>
+        </li>
+
+        <li>
+          <strong>Personal Responsible: </strong> <?= $new_tasks['assigned_to']; ?>
+        </li>
+        <li>
+          <?php if ($new_tasks['completed']) : ?>
+            <span class="icon">&#9989;</span>
+          <?php else : ?>
+            <span class="icon">Incomplete</span>
+          <?php endif; ?>
+        </li>
+      </ul>
+    <?php endif; ?>
+
   </body>
 
 </html>
